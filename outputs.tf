dc3688e73acb340d1ab5f051c121de656e098158
flaviomrjr <flavio.rocha16@gmail.com>
2023-01-28 14:48:35 -0300

    feat: add output private app sub

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "db_private_sub_ids" {
   description = "Output DB Private Subnets IDs"
   value       = aws_subnet.private_subnet_db[*].id
 }
+
+output "app_private_vpc_id" {
+  description = "Output APP Private VPC ID"
+  value       = aws_vpc.vpc_app.id
+}
+
+output "app_private_sub_ids" {
+  description = "Output APP Private Subnets IDs"
+  value       = aws_subnet.private_subnet_app[*].id
+}
