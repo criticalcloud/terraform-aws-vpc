1dac7475cc7873ca265f4c79a3a505b7f8645897
flaviomrjr <flavio.rocha16@gmail.com>
2023-01-28 14:05:34 -0300

    fix: outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "db_private_vpc_id" {
   description = "Output DB Private VPC ID"
-  value       = [aws_vpc.vpc_db.id]
+  value       = aws_vpc.vpc_db.id
 }
 
 output "db_private_sub_ids" {
   description = "Output DB Private Subnets IDs"
-  value       = [aws_subnet.private_subnet_db[0].id,aws_subnet.private_subnet_db[1].id,aws_subnet.private_subnet_db[2].id]
+  value       = aws_subnet.private_subnet_db[*].id
 }
