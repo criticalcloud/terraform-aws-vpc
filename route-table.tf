d9f5716d71a5dae1305e7f32b779cbb8d6b0164a
flaviomrjr <flaviomrjr@gmail.com>
2024-04-14 12:43:29 -0300

    chore: fix route table

--- route-table.tf
+++ route-table.tf
@@ -3,33 +3,36 @@ resource "aws_route_table" "route_private_db" {
   count  = var.create_vpc_db == true ? 1 : 0
   vpc_id = aws_vpc.vpc_db[count.index].id
 
-  route {
-    cidr_block                = var.cidr_vpc_app
-    vpc_peering_connection_id = aws_vpc_peering_connection.px_app_db[count.index].id
-  }
-
   tags = {
     Name = "${var.private_subnet_db_name}"
   }
+}
+
+resource "aws_route" "route_private_db_01" {
+  route_table_id            = aws_route_table.route_private_db.id
+  destination_cidr_block    = var.cidr_vpc_app
+  vpc_peering_connection_id =  aws_vpc_peering_connection.px_app_db[count.index].id
 }
 
 # ROUTE TABLE VPC app SUBNETES PRIVADAS
 resource "aws_route_table" "route_private_app" {
   vpc_id = aws_vpc.vpc_app.id
 
-  route {
-    cidr_block                = var.cidr_vpc_db
-    vpc_peering_connection_id = aws_vpc_peering_connection.px_app_db[0].id
-  }
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ng_app[0].id
-  }
-
   tags = {
     Name = "${var.private_subnet_app_name}"
   }
+}
+
+resource "aws_route" "route_private_app_01" {
+  route_table_id            = aws_route_table.route_private_app.id
+  destination_cidr_block    = var.cidr_vpc_db
+  vpc_peering_connection_id = aws_vpc_peering_connection.px_app_db[0].id
+}
+
+resource "aws_route" "route_private_app_02" {
+  route_table_id            = aws_route_table.route_private_app.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.ng_app[0].id
 }
 
 # ROUTE TABLE VPC app SUBNETES PUBLICAS
@@ -37,14 +40,15 @@ resource "aws_route_table" "route_public_app" {
   count  = var.create_public_sub == true ? 1 : 0
   vpc_id = aws_vpc.vpc_app.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ig_app.id
-  }
-
   tags = {
     Name = "${var.public_subnet_app_name}"
   }
+}
+
+resource "aws_route" "route_public_app_01" {
+  route_table_id            = aws_route_table.route_public_app.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.ig_app.id
 }
 
 # ADD ROUTE TABLE SUBNETS PRIVADAS DB
